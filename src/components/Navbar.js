5abb4642c0499cb88257d46ac3a59aceb5b4e809
Walter Aab <aabwalter@yahoo.com>
2021-08-09 14:39:25 -0500

    wip Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,26 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
 
-const Navbar = () => {
+
+const Navbar = (props) => {
     return (
-        
+        <div class="navbar navbar-expand-lg navbar-light bg-light">
+            <NavLink
+            to='/'
+            exact
+            class="navbar-brand"
+            >Logo</NavLink>
+            <NavLink
+            to="/trees/new"
+            exact
+            class="nav-item"
+            >Add location</NavLink>
+            <NavLink
+            to="/map"
+            exact
+            class="nav-item"
+            >Map</NavLink>
+        </div>
     )
 }
 
