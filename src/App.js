540c220976c0adf21b55b3f9e4d6433d558cb40b
Walter Aab <aabwalter@yahoo.com>
2021-08-08 20:52:03 -0500

    Move MapContainer to its own file

--- src/App.js
+++ src/App.js
@@ -1,53 +1,64 @@
 import React, { Component } from 'react';
-import { GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
-import CurrentLocation from './Map';
+import MapContainer from './containers/MapContainer';
 import TreeForm from './components/trees/TreeForm';
-import env from 'react-dotenv';
 
-
-export class MapContainer extends Component {
-  state = {
-    showingInfoWindow: false,
-    activeMarker: {},
-    selectedPlace: {}
-  };
-
-  onMarkerClick = (props, marker, e) =>
-    this.setState({
-      selectedPlace: props,
-      activeMarker: marker,
-      showingInfoWindow: true
-    });
-
-  onClose = props => {
-    if (this.state.showingInfoWindow) {
-      this.setState({
-        showingInfoWindow: false,
-        activeMarker: null
-      });
-    }
-  };
-
+class App extends Component {
   render() {
-    return (
-      <CurrentLocation
-      centerAroundCurrentLocation
-      google={this.props.google}
-      >
-        <Marker onClick={this.onMarkerClick} name={'Current Location'} />
-        <InfoWindow
-          marker={this.state.activeMarker}
-          visible={this.state.showingInfoWindow}
-          onClose={this.onClose}
-        >
-          <div>
-            <h4>{this.state.selectedPlace.name}</h4>
-          </div>
-        </InfoWindow>
-      </CurrentLocation>,
-      <TreeForm />
-    );
+    return(
+      <div className="App">
+        <TreeForm />
+        <MapContainer />
+      </div>
+    )
   }
+
 }
 
-export default GoogleApiWrapper({ apiKey: env.API_KEY })(MapContainer);
+export default App
+
+// export class MapContainer extends Component {
+//   state = {
+//     showingInfoWindow: false,
+//     activeMarker: {},
+//     selectedPlace: {}
+//   };
+
+//   onMarkerClick = (props, marker, e) =>
+//     this.setState({
+//       selectedPlace: props,
+//       activeMarker: marker,
+//       showingInfoWindow: true
+//     });
+
+//   onClose = props => {
+//     if (this.state.showingInfoWindow) {
+//       this.setState({
+//         showingInfoWindow: false,
+//         activeMarker: null
+//       });
+//     }
+//   };
+
+//   render() {
+//     return (
+//       <CurrentLocation
+//       centerAroundCurrentLocation
+//       google={this.props.google}
+//       >
+//         <Marker onClick={this.onMarkerClick} name={'Current Location'} />
+//         <InfoWindow
+//           marker={this.state.activeMarker}
+//           visible={this.state.showingInfoWindow}
+//           onClose={this.onClose}
+//         >
+//           <div>
+//             <h4>{this.state.selectedPlace.name}</h4>
+//           </div>
+//         </InfoWindow>
+//       </CurrentLocation>,
+//       <TreeForm />
+//     );
+//   }
+// }
+
+// export default GoogleApiWrapper({ apiKey: env.API_KEY })(MapContainer);
