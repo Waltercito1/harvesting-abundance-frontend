65fe45ea9259c6bf8006d832e840de14fc293f12
Walter Aab <aabwalter@yahoo.com>
2021-08-16 11:09:08 -0500

    Remove unused code

--- src/components/Login.js
+++ src/components/Login.js
@@ -26,44 +26,6 @@ class Login extends React.Component {
 
   render() {
     return (
-    //   <form
-    //     onSubmit={this.handleSubmit}
-    //     className='w-11/12 max-w-2xl mx-auto mt-8'
-    //   >
-    //     <h1 className='font-bold text-3xl'>Log In</h1>
-    //     <p className="h-8 text-red-400">{this.state.error && "Invalid email or password"}</p>
-    //     <fieldset>
-    //       <label className='block uppercase mb-2' htmlFor='email'>
-    //         Email:
-    //       </label>
-    //       <input
-    //         type='text'
-    //         name='email'
-    //         id='email'
-    //         className='w-full border-2 focus:outline-none focus:ring-2 p-4 mb-4'
-    //         onChange={this.handleChange}
-    //         value={this.state.email}
-    //       />
-    //     </fieldset>
-    //     <fieldset>
-    //       <label className='block uppercase mb-2' htmlFor='password'>
-    //         Password:
-    //       </label>
-    //       <input
-    //         type='password'
-    //         name='password'
-    //         id='password'
-    //         className="w-full border-2 focus:outline-none focus:ring-2 p-4 mb-4"
-    //         onChange={this.handleChange}
-    //         value={this.state.password}
-    //       />
-    //     </fieldset>
-    //     <input
-    //       className='w-full text-center uppercase p-4 bg-blue-300 cursor-pointer mt-4'
-    //       type='submit'
-    //       value='Log In'
-    //     />
-    //   </form>,
       <div class="registration-form">
       <form onSubmit={this.handleSubmit}>
           <div className="form-icon">
