ba28a36791d10a9be83aea5566f1a74431eb264b
Walter Aab <aabwalter@yahoo.com>
2021-08-16 10:57:32 -0500

    Add footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <div className="footer" id="page-content">
+            <footer className="container flex-shrink-0 py-2 bg-light text-gray-50">
+            <div className="text-center">
+                <small>Copyright &copy; Harvesting Abundance</small>
+            </div>
+            </footer>
+        </div>
+    )
+}
+
+export default Footer
