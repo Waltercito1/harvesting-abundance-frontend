300b30571458903ebe475c0103c83c6cf91ef9c6
Walter Aab <aabwalter@yahoo.com>
2021-08-16 10:57:01 -0500

    Add about page

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+//import TreeForm from "./trees/TreeForm"
+
+const About = () => {
+    return (
+        <div>
+            About page.
+            {/* < TreeForm /> */}
+        </div>
+    )
+}
+
+export default About
