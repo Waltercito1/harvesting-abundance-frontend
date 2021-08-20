a2b42ef49975cedc4b7cfa2fcc3774ced48e0b2a
Walter Aab <aabwalter@yahoo.com>
2021-08-20 17:48:33 -0500

    Correct class for className/ delete tree wip

--- src/components/trees/ShowTree.js
+++ src/components/trees/ShowTree.js
@@ -6,7 +6,6 @@ import { removeTree } from '../../actions/index'
 const ShowTree = (props) => {
     debugger
 
-
     const handleClick = () => {
         removeTree(props.location.state.id)
         props.history.push("/trees")
@@ -14,35 +13,35 @@ const ShowTree = (props) => {
 
     const formatImg = () => props.location.state.image_format ? props.location.state.image_format.url : "https://harvesting-abundance.s3.us-east-2.amazonaws.com/No-Image-Placeholder.svg"
     
-    const conditionalBttns = () => props.location.state === "/" ? <></> : <button className="btn btn-danger" onClick={() => handleClick()}>Delete</button>
+    const conditionalBttn = () => props.location.state.users[0].id !== 1 ? <></> : <button className="btn btn-danger" onClick={() => handleClick()}>Delete</button>
 
     return(
         <div>
         <Container fluid >
-            <div class="row mb-4 align-items-center">
-                <div class="col-md-6 aos-init aos-animate">
+            <div className="row mb-4 align-items-center">
+                <div className="col-md-6 aos-init aos-animate">
                     <h2>{props.location.state.name}</h2>
                     <p>{props.location.state.description}</p>
                 </div>
             </div>
-            <div class="site-section pb-0">
+            <div className="site-section pb-0">
                 <div >
-                <div class="row align-items-stretch">
-                    <div class="col-md-8 aos-init aos-animate" >
-                        <img src={formatImg()} alt="treeImage" class="img-fluid" width={600}style={{objectFit: 'cover', padding: '0 0 0 1em'}}/>
+                <div className="row align-items-stretch">
+                    <div className="col-md-8 aos-init aos-animate" >
+                        <img src={formatImg()} alt="treeImage" className="img-fluid" width={600}style={{objectFit: 'cover', padding: '0 0 0 1em'}}/>
                     </div>
-                    <div class="col-md-3 ml-auto" >
-                    <div class="sticky-content">
-                        <h3 class="h3">Location</h3>
-                        <p class="mb-4"><span class="text-muted">Latitude: {props.location.state.locations[0].latitude}</span></p>
-                        <p class="mb-4"><span class="text-muted">Longitude: {props.location.state.locations[0].longitude}</span></p>
+                    <div className="col-md-3 ml-auto" >
+                    <div className="sticky-content">
+                        <h3 className="h3">Location</h3>
+                        <p className="mb-4"><span className="text-muted">Latitude: {props.location.state.locations[0].latitude}</span></p>
+                        <p className="mb-4"><span className="text-muted">Longitude: {props.location.state.locations[0].longitude}</span></p>
 
-                        <div class="mb-5">
+                        <div className="mb-5">
                         <p>Show small Map Conatiner with location.</p>
                         </div>
 
-                        <h4 class="h4 mb-3">Details:</h4>
-                        <ul class="list-unstyled list-line mb-5">
+                        <h4 className="h4 mb-3">Details:</h4>
+                        <ul className="list-unstyled list-line mb-5">
                         <li>Access Type</li>
                         <li>Season</li>
                         <li>Date Visited</li>
@@ -51,8 +50,9 @@ const ShowTree = (props) => {
                         <li>Yield</li>
                         </ul>
 
-                        <button type="button" class="btn btn-info">Edit</button><br /><br />
-                        <button type="button" class="btn btn-danger">Delete</button>
+                        <button type="button" className="btn btn-info">Edit</button><br /><br />
+                        <button type="button" className="btn btn-danger">Delete</button>
+                        {conditionalBttn()}
                     </div>
                     </div>
                 </div>
